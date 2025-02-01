76ce21ff08ad97ea33d6594b752d047e4f90937d
Jay Hannah <jay.hannah@iinteractive.com>
2025-02-01 09:38:25 -0600

    I need to disable eleventyImageTransformPlugin
    to get base.njk og:image working?

--- eleventy.config.js
+++ eleventy.config.js
@@ -2,7 +2,7 @@ import { IdAttributePlugin, InputPathToUrlTransformPlugin, HtmlBasePlugin } from
 import { feedPlugin } from "@11ty/eleventy-plugin-rss";
 import pluginSyntaxHighlight from "@11ty/eleventy-plugin-syntaxhighlight";
 import pluginNavigation from "@11ty/eleventy-navigation";
-import { eleventyImageTransformPlugin } from "@11ty/eleventy-img";
+//import { eleventyImageTransformPlugin } from "@11ty/eleventy-img";
 
 import pluginFilters from "./_config/filters.js";
 
@@ -72,26 +72,27 @@ export default async function(eleventyConfig) {
 		}
 	});
 
+	// I don't want/need this? I just want my og:image to work:
 	// Image optimization: https://www.11ty.dev/docs/plugins/image/#eleventy-transform
-	eleventyConfig.addPlugin(eleventyImageTransformPlugin, {
+	//eleventyConfig.addPlugin(eleventyImageTransformPlugin, {
 		// Output formats for each image.
-		formats: ["avif", "webp", "auto"],
+		//formats: ["avif", "webp", "auto"],
 
 		// widths: ["auto"],
 
-		failOnError: false,
-		htmlOptions: {
-			imgAttributes: {
+		//failOnError: false,
+		//htmlOptions: {
+		//	imgAttributes: {
 				// e.g. <img loading decoding> assigned on the HTML tag will override these values.
-				loading: "lazy",
-				decoding: "async",
-			}
-		},
+		//		loading: "lazy",
+		//		decoding: "async",
+		//	}
+		//},
 
-		sharpOptions: {
-			animated: true,
-		},
-	});
+		//sharpOptions: {
+		//	animated: true,
+		//},
+	//});
 
 	// Filters
 	eleventyConfig.addPlugin(pluginFilters);
